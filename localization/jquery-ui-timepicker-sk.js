4f0eb62c393a603d8beff69e124a3f1f34ce7285
Jan Novotný <novoj@jnonb.(none)>
2012-01-27 10:48:05 +0100

    Added Slovak localization.

---
+++ localization/jquery-ui-timepicker-sk.js
@@ -0,0 +1,19 @@
+/* Slovak translation for the jQuery Timepicker Addon */
+/* Written by Andrea Pavolková */
+(function($) {
+    $.timepicker.regional['sk'] = {
+        timeOnlyTitle: 'Vyberte čas',
+        timeText: 'Čas',
+        hourText: 'Hodiny',
+        minuteText: 'Minúty',
+        secondText: 'Sekundy',
+        timezoneText: 'Časové pásmo',
+        currentText: 'Teraz',
+        closeText: 'Zavrieť',
+        timeFormat: 'h:m',
+        ampm: false,
+        am: 'dop.',
+        pm: 'odp.'
+    };
+    $.timepicker.setDefaults($.timepicker.regional['sk']);
+})(jQuery);
